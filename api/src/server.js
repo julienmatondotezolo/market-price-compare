23308994c28ae887174ccdb278d29988faea66fd
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2020-12-28 21:58:15 +0100

    refactor(helpers.js):Adding helpers file for uuid

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("http");
-// const Helpers = require("./utils/helpers.js");
+const Helpers = require("./utils/helpers.js");
 var path = require("path");
 const port = 3000;
 const htmlFile = path.join(__dirname + "/components/index.html");
---
+++ api/src/utils/helpers.js
@@ -0,0 +1,10 @@
+const {v1: uuidv1 } = require('uuid');
+
+const Helpers = {
+  generateUUID: () => {
+     const uuid = uuidv1();  
+     return uuid;
+  }
+}
+
+module.exports = Helpers
