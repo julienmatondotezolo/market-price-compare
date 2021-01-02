32cd3c0224102a230c93e9aec5883dad121b83b6
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-01-02 22:28:25 +0100

    refactor(server):scrapping alberthein

--- api/src/server.js
+++ api/src/server.js
@@ -5,6 +5,8 @@ const http = require("http");
 const Helpers = require("./utils/helpers.js");
 var path = require("path");
 const port = 3000;
+const puppeteer = require("puppeteer");
+const $ = require("cheerio");
 
 const pg = require("knex")({
     client: "pg",
@@ -23,11 +25,88 @@ app.use(
     })
 );
 
-// ========== SHOW ALL RECORDS ==========  //
 app.get("/", (req, res) => {
-    res.status(200).send(200);
+    res.sendStatus(200).send(200);
 });
 
+// ========== GET ALL SHOPS ==========  //
+app.get("/shop/:query", async (req, res, next) => {
+    let query = req.params.query;
+    storeArr = [];
+    try {
+        await getShopItems(res, query);
+        res.send(`Searching prices for: ${query}.`);
+    } catch (err) {
+        res.end();
+        next(err);
+    }
+});
+
+// ========== SEARCH ITEM IN ALL SHOPS ==========  //
+async function getShopItems(res, query) {
+    try {
+        // AlbertHein
+        await checkAlbertHein(res);
+        // res.json(storeArr);
+        // res.end();
+    } catch (error) {
+        throw error;
+    }
+}
+
+// ========== SEARCH ITEM AIN ALBERTHEIN ==========  //
+async function checkAlbertHein(res) {
+    const base_url = "https://www.ah.be";
+    const url = `https://www.ah.be/producten/frisdrank-sappen-koffie-en-thee/frisdrank?page=1`;
+
+    const browser = await puppeteer.launch({
+        // headless: false
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+        waitUntil: 'networkidle2'
+    });
+
+    let data = await page.evaluate(() => { 
+        let list = document.querySelectorAll('#search-lane > div');
+        let productArray = [];
+
+        for (let e = 0; e < list.length; e++) {
+            let productList = document.querySelectorAll(`#search-lane > div:nth-child(${e}) article`);
+            for (let i = 1; i < productList.length; i++) {
+                let productObj = {};
+
+                let product_name = document.querySelector(`#search-lane > div:nth-child(${e}) article:nth-child(${i}) > div+div`);
+                let product_image = document.querySelector(`#search-lane > div:nth-child(${e}) article:nth-child(${i}) .lazy-image_image__2025k`);
+                let product_description = document.querySelector(`#search-lane > div:nth-child(${e}) article:nth-child(${i}) .price_unitSize__8gRVX`);
+                let product_details = document.querySelector(`#search-lane > div:nth-child(${e}) article:nth-child(${i}) a`);
+                let product_price = document.querySelector(`#search-lane > div:nth-child(${e}) article:nth-child(${i}) .price-amount_root__37xv2`);
+                let product_category = document.querySelector('h1#start-of-content');
+                let product_shop_id = '';
+
+                productObj.product_name = product_name ? product_name.innerText : '';
+                productObj.product_image = product_image ? product_image.src : '';
+                productObj.product_description = product_description ? product_description.innerText : '';
+                productObj.product_details = product_details ? product_details.href : '';
+                productObj.product_price = product_price ? product_price.innerText : '';
+                productObj.product_category = product_category ? product_category.innerText : '';
+                productObj.product_shop_id = product_shop_id ? product_shop_id.innerText : '';
+                productArray.push(productObj);
+            }
+       }
+
+        return productArray;
+    })
+    await browser.close();
+    console.log(data);
+    // res.send(data)
+}
+
+getShopItems();
+
+// ========== DATABASE ==========  //
 const shopItemsSeeders = async () => {
     let productItemObj = {
         uuid: Helpers.generateUUID(),
@@ -41,15 +120,15 @@ const shopItemsSeeders = async () => {
     }
 
     const shopItems = await pg
-    .table("market")
-    .insert(productItemObj)
-    .then(async function () {
-        console.log("✅", "Market items seeders");
-        return;
-    })
-    .catch((e) => {
-        console.log("Error occured: ", e);
-    });
+        .table("market")
+        .insert(productItemObj)
+        .then(async function () {
+            console.log("✅", "Market items seeders");
+            return;
+        })
+        .catch((e) => {
+            console.log("Error occured: ", e);
+        });
 }
 
 const shopSeeders = async () => {
@@ -61,24 +140,16 @@ const shopSeeders = async () => {
     }
 
     const shopItems = await pg
-    .table("shops")
-    .insert(shopObj)
-    .then(async function () {
-        console.log("✅", "Shop seeders");
-        return;
-    })
-    .catch((e) => {
-        console.log("Error occured: ", e);
-    });
+        .table("shops")
+        .insert(shopObj)
+        .then(async function () {
+            console.log("✅", "Shop seeders");
+            return;
+        })
+        .catch((e) => {
+            console.log("Error occured: ", e);
+        });
 }
-
-// product_uuid: Helpers.generateUUID()
-// product_name: shopItem.name,
-// product_description: shopItem.description,
-// product_details: shopItem.details,
-// product_price: shopItem.price,
-// product_category: shopItem.category,
-// product_shopId: shopItem.shopId,
 
 // ========== INIT TABLES ==========  //
 async function initialiseTables() {
